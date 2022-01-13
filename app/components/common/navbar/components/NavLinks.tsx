1b82395e67f880ad2aa739f5e14dfef1c4fa8282
Alex Nicholas <me@alexnicholas.dev>
2022-01-13 10:13:14 +0000

    comment out currently unused keyboard navigation logic

--- app/components/common/navbar/components/NavLinks.tsx
+++ app/components/common/navbar/components/NavLinks.tsx
@@ -10,35 +10,35 @@ import {
 } from '~/components/icons';
 
 export function NavLinks() {
-	const navLinkRefs = React.useRef<HTMLElement[]>([]);
+	// const navLinkRefs = React.useRef<HTMLElement[]>([]);
 
-	const navigateWithArrowKeys = (e: KeyboardEvent) => {
-		const activeElement = document.activeElement as HTMLElement;
-		const activeIndex = activeElement.dataset['index'];
-		const lastIndex = navLinkRefs.current.length - 1;
+	// const navigateWithArrowKeys = (e: KeyboardEvent) => {
+	// 	const activeElement = document.activeElement as HTMLElement;
+	// 	const activeIndex = activeElement.dataset['index'];
+	// 	const lastIndex = navLinkRefs.current.length - 1;
 
-		if (e.key === 'ArrowDown') {
-			const nextIndex =
-				activeIndex && Number(activeIndex) !== lastIndex
-					? Number(activeIndex) + 1
-					: 0;
-			navLinkRefs.current[nextIndex].focus();
-			return;
-		}
-		if (e.key === 'ArrowUp') {
-			const previousIndex =
-				activeIndex && Number(activeIndex) !== 0
-					? Number(activeIndex) - 1
-					: lastIndex;
-			navLinkRefs.current[previousIndex].focus();
-			return;
-		}
-	};
+	// 	if (e.key === 'ArrowDown') {
+	// 		const nextIndex =
+	// 			activeIndex && Number(activeIndex) !== lastIndex
+	// 				? Number(activeIndex) + 1
+	// 				: 0;
+	// 		navLinkRefs.current[nextIndex].focus();
+	// 		return;
+	// 	}
+	// 	if (e.key === 'ArrowUp') {
+	// 		const previousIndex =
+	// 			activeIndex && Number(activeIndex) !== 0
+	// 				? Number(activeIndex) - 1
+	// 				: lastIndex;
+	// 		navLinkRefs.current[previousIndex].focus();
+	// 		return;
+	// 	}
+	// };
 
-	React.useEffect(() => {
-		document.addEventListener('keydown', navigateWithArrowKeys);
-		return () => document.removeEventListener('keydown', navigateWithArrowKeys);
-	}, []);
+	// React.useEffect(() => {
+	// 	document.addEventListener('keydown', navigateWithArrowKeys);
+	// 	return () => document.removeEventListener('keydown', navigateWithArrowKeys);
+	// }, []);
 
 	return (
 		<>
@@ -47,7 +47,7 @@ export function NavLinks() {
 					<li key={page}>
 						<Link
 							to={page === 'home' ? '/' : `/${page}`}
-							ref={(el) => el && (navLinkRefs.current[index] = el)}
+							// ref={(el) => el && (navLinkRefs.current[index] = el)}
 							data-index={index}
 							className="capitalize flex items-center gap-4"
 						>
