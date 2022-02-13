3db126d461e7014787095aaf5da1c8fc928a72ae
Alex Nicholas <me@alexnicholas.dev>
2022-02-13 15:25:38 +0000

    update csp

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -29,6 +29,6 @@ export default function handleRequest(
 	});
 }
 
-const baseCSP = `default-src 'none'; img-src 'self'; style-src 'self' http://fonts.googleapis.com; font-src https://fonts.gstatic.com;base-uri 'self'; form-action 'self';`;
-const devCSP = `script-src 'unsafe-inline' 'self';`;
+const baseCSP = `default-src 'none'; img-src 'self'; style-src 'self' 'unsafe-inline' http://fonts.googleapis.com; font-src https://fonts.gstatic.com;base-uri 'self'; form-action 'self';`;
+const devCSP = `script-src 'unsafe-inline' 'self'; connect-src ws://localhost:8002;`;
 const prodCSP = `script-src 'self' 'nonce-${nonce}'; connect-src 'self';`;
