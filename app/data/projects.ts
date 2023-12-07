9bdb086af871c65c97ffdc6b1574039e294af208
Alex Nicholas <me@alexnicholas.dev>
2023-12-07 19:15:46 +0000

    update whiskybored url

--- app/data/projects.ts
+++ app/data/projects.ts
@@ -175,7 +175,7 @@ export const projects: Project[] = [
 		published: true,
 		image: null,
 		thumbnail: null,
-		url: 'https://whiskybored-production.up.railway.app',
+		url: 'https://whiskybored.fly.dev/',
 		githubPrimary: 'https://github.com/alicnik/whiskybored',
 		responsive: false,
 		projectDate: new Date(2021, 9),
